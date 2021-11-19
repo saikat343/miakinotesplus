98c370e6d61651a3888c91f5eee270b45c8c0278
Saikat <Saikat>
2021-11-20 00:53:49 +0600

    Djoser setup

--- backend/miakinotesplus/settings.py
+++ backend/miakinotesplus/settings.py
@@ -158,7 +158,12 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {
+        'user_create': 'accounts.serializers.UserCreateSerializer',
+        'user': 'accounts.serializers.UserCreateSerializer',
+        'user_delete': 'djoser.serializers.UserDeleteSerializer',
+    }
 }
 
 AUTH_USER_MODEL = 'accounts.UserAccount'
